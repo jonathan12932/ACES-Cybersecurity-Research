f4654c323431e90d5e77b1d620894e81067ed28d
Jonathan Lee <74516455+jonathan12932@users.noreply.github.com>
2024-12-02 21:47:50 -0500

    Update AttackerCountHour.sh

--- Scripts/AttackerCountHour.sh
+++ Scripts/AttackerCountHour.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the log file path
-LOG_FILE="/home/lxc_list_log.txt"
+LOG_FILE="/home/lxc_list_log_hour.txt"
 
 # Print the current date and time as a header for each entry
 echo "----- $(date +'%Y-%m-%d %H:%M:%S') -----" >> "$LOG_FILE"
 
 # Run 'sudo lxc-ls' and append the output to the log file
 sudo lxc-ls >> "$LOG_FILE"
-sudo bash reboot.sh
+
 # Add a newline for readability
 echo "" >> "$LOG_FILE"
